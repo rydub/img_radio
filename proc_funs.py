9df88f91f97a1ae9132f88c1b6c3d8c6e12f0fa0
Ryan Williams <solafirearts@gmail.com>
2018-05-04 01:10:20 -0700

    tabs to spaces my ass

--- proc_funs.py
+++ proc_funs.py
@@ -31,31 +31,31 @@ def grayscale_proc():
 
 def color_proc():
 
-	def for_proc_fun(img_path):
-		test_img = scipy.ndimage.imread(img_path)
-		channels = [test_img[:,:,0], test_img[:,:,1], test_img[:,:,2]]
-		byte_list = []
-    	shapes = []
-		for channel in channels:
-			curr_shape = channel.shape
-			shapes.append(curr_shape)
-			flattened_channel = np.reshape(channel, (curr_shape[0]*curr_shape[1]))
-			curr_bytes = flattened_channel.tobytes()
-			byte_list.append(curr_bytes)
-		all_bytes = b''.join(byte_list)
-		return (all_bytes, shapes)
+    def for_proc_fun(img_path):
+        test_img = scipy.ndimage.imread(img_path)
+        channels = [test_img[:,:,0], test_img[:,:,1], test_img[:,:,2]]
+        byte_list = []
+        shapes = []
+        for channel in channels:
+            curr_shape = channel.shape
+            shapes.append(curr_shape)
+            flattened_channel = np.reshape(channel, (curr_shape[0]*curr_shape[1]))
+            curr_bytes = flattened_channel.tobytes()
+            byte_list.append(curr_bytes)
+        all_bytes = b''.join(byte_list)
+        return (all_bytes, shapes)
 
 
-	def rev_proc_fun(rec_img_path, img_bytes, shapes):
-		flattend_channels = np.frombuffer(bytes_str, dtype = np.uint8)
-    	channels = []
-    	chn_idx = 0
-    	for shape in shapes:
-        	chn_end = chn_idx + shape[0]*shape[1]
-        	flat_chn = flattend_channels[chn_idx:chn_end]
-        	channel = np.reshape(flat_chn, shape)
-        	channels.append(channel)
-        	chn_idx = chn_end
-    	scipy.misc.imsave(rec_img_path, channels)
+    def rev_proc_fun(rec_img_path, img_bytes, shapes):
+        flattend_channels = np.frombuffer(bytes_str, dtype = np.uint8)
+        channels = []
+        chn_idx = 0
+        for shape in shapes:
+            chn_end = chn_idx + shape[0]*shape[1]
+            flat_chn = flattend_channels[chn_idx:chn_end]
+            channel = np.reshape(flat_chn, shape)
+            channels.append(channel)
+            chn_idx = chn_end
+        scipy.misc.imsave(rec_img_path, channels)
 
 
